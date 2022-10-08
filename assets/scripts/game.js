b3ae54cc13515b45a6af100e3f0cf102b4bf7c43
dragonishare <dragonishare@gmail.com>
2022-10-08 16:03:03 +0800

    judge result

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -5,6 +5,14 @@ cc.Class({
         rope_node: {
             default: null,
             type: cc.Node
+        },
+        cow_ins: {
+            default: null,
+            type: cc.Node
+        },
+        rope_imgs: {
+            default: [],
+            type:cc.SpriteFrame
         }
     },
 
@@ -32,6 +40,26 @@ cc.Class({
         this.rope_node.y = -480;
         // 向上移动
         const up = cc.moveTo(0.5, this.rope_node.x, 0);
-        this.rope_node.runAction(up);
+
+        // 判断结果
+        let result = cc.callFunc(function () {
+            // 获取当前牛的x点
+            let currentX = this.cow_ins.x;
+            if (currentX > -100 && currentX < 100) {
+                cc.log("捕捉成功！");
+                // 更换绳子
+                let ropeType = this.cow_ins.getComponent("cow").randomType + 1;
+                this.rope_node.getComponent(cc.Sprite).spriteFrame = this.rope_imgs[ropeType];
+            } else {
+                cc.log("捕捉失败！")
+            }
+        }, this);
+
+        // 往回拉
+        let down = cc.moveTo(0.5, this.rope_node.x, -600);
+
+        // 定义一个序列动画
+        let sequence = cc.sequence(up, result,down);
+        this.rope_node.runAction(sequence);
     }
 });
